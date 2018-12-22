79a367288fefb8d600c8cd169359532ffae2d8df
Fumnanya Agbimson <fumnanyaagbimson@Fumnanyas-MacBook-Pro.local>
2018-12-22 12:29:21 -0500

    v2

---
+++ javascript/game.js
@@ -0,0 +1,8 @@
+var redGem =   Math.floor(Math.random() * 10);  
+  var greenGem = Math.floor(Math.random() * 10);  
+  var blueGem = Math.floor(Math.random() * 10);  
+  var orangeGem = Math.floor(Math.random() * 10);  
+
+  var win = 0;
+  var lose = 0;
+  var total = 0;
